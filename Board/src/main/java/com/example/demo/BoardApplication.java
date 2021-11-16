56fc72c080ca372ee70b9adefa17b99f6a92c4b3
miryong <nkiller77@naver.com>
2021-11-16 21:32:13 +0900

    Test Commit

--- Board/src/main/java/com/example/demo/BoardApplication.java
+++ Board/src/main/java/com/example/demo/BoardApplication.java
@@ -3,10 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class BoardApplication {
 
 	public static void main(String[] args) {
+		//TEST COMMIT
+		System.out.println("TEST!!");
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
